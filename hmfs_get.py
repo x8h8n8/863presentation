c3200e2704092f17edf907a1276cc28dae7b7323
x8h8n8 <x8h8n8@163.com>
2017-09-25 16:35:11 +0800

    debug

--- hmfs_get.py
+++ hmfs_get.py
@@ -9,12 +9,13 @@ cur = db.cursor()
 
 def getHmfs():
     with open('./hmfs_status.txt') as f:
-        hmfs_use = int(f.readline().split()[1])
-        hmfs_t = int(f.readline().split()[2])
-        t = int(time.time())
-        sql = 'insert into hmfs_info (time,hmfs,hmfs_total) value (%s,%s,%s)'%(t,hmfs_use,hmfs_t)
-        cur.execute(sql)
-        print t,hmfs_use,hmfs_t
+        l = f.readline().split()
+        hmfs_use = int(l[1])
+        hmfs_t = int(l[2])
+    t = int(time.time())
+    sql = 'insert into hmfs_info (time,hmfs,hmfs_total) value (%s,%s,%s)'%(t,hmfs_use,hmfs_t)
+    cur.execute(sql)
+    print t,hmfs_use,hmfs_t
         #print 'ok'
 while True:
     time.sleep(1)
