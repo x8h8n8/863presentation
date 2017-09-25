60295415c8bc70f3eb80f21771e57cd49d4cb973
Kaixin <Meditator.hkx@gmail.com>
2017-09-25 17:17:21 +0800

    debug dram hmfs

--- hmfs_get.py
+++ hmfs_get.py
@@ -11,23 +11,12 @@ def getHmfs():
     with open('./hmfs_status.txt') as f:
         l = f.readline().split()
         hmfs_use = int(l[1])
-<<<<<<< HEAD
-        print hmfs_use
-        hmfs_t = int(l[2])
-        print hmfs_t
-    	t = int(time.time())
-    	sql = 'insert into hmfs_info (time,hmfs,hmfs_total) value (%s,%s,%s)'%(t,hmfs_use,hmfs_t)
-    	cur.execute(sql)
-    	print t,hmfs_use,hmfs_t
-    	 #print 'ok'
-=======
         hmfs_t = int(l[2])
     t = int(time.time())
     sql = 'insert into hmfs_info (time,hmfs,hmfs_total) value (%s,%s,%s)'%(t,hmfs_use,hmfs_t)
     cur.execute(sql)
     print t,hmfs_use,hmfs_t
-        #print 'ok'
->>>>>>> c3200e2704092f17edf907a1276cc28dae7b7323
+
 while True:
     time.sleep(1)
     getHmfs()
